91d9412771d1b6f9e8dd3877f001201713a9f776
vilashbuddh <buddh.singh@ziploan.in>
2020-07-12 13:33:57 +0530

    feat(Add check on userid while checking token validity)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,17 @@
 
 //Middleware responsible for checking the token validity
 var Session = require('../models/session')
+var ObjectId = require('mongodb').ObjectId
 
 module.exports = function(req, res, next) {
 
   const token = req.headers.token
+  const userId = req.headers.userid
 
-  if (!token) return res.status(401).json({ message: "Auth Error, Missing token" });
+  if (!token || !userId) return res.status(401).json({ message: "Auth Error, Missing token or userId" });
 
-  Session.findOne({token: token}, function(err, result){
-    if (! result){
+  Session.findOne({userId: ObjectId(userId)}, function(err, result){
+    if (result.token != token){
       return res.status(401).json({msg: 'Invalid token'})
     }
     next();
